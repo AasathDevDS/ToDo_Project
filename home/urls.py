9d65a471c8a6fddceebd576b7415291092ece7c3
AasathDevDS <aasatheng2003@gmail.com>
2025-11-27 13:41:55 +0530

    Updated ToDo editor page: modern style and responsive design

--- home/urls.py
+++ home/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
   path('addTask/', views.addTask, name = "addTask" ),
-  path('mark_as_read/<int:pk>', views.mark_as_read, name = 'mark_as_read'),
+  path('mark_as_done/<int:pk>', views.mark_as_done, name = 'mark_as_done'),
   path('delete/<int:pk>', views.delete, name = 'delete'),
-  path('mark_as_unread/<int:pk>', views.mark_as_unread, name = 'mark_as_unread'),
+  path('mark_as_undone/<int:pk>', views.mark_as_undone, name = 'mark_as_undone'),
   path('edit_task/<int:pk>', views.edit_task, name = 'edit_task'),
 ]
--- home/views.py
+++ home/views.py
@@ -9,7 +9,7 @@ def addTask(request):
   Task.objects.create(task = task)
   return redirect('home')
 
-def mark_as_read(request, pk):
+def mark_as_done(request, pk):
   task = get_object_or_404(Task, pk = pk)
   task.is_completed = True
   task.save()
@@ -20,7 +20,7 @@ def delete(request, pk):
   task.delete()
   return redirect('home')
 
-def mark_as_unread(request, pk):
+def mark_as_undone(request, pk):
   task = get_object_or_404(Task, pk = pk)
   task.is_completed = False
   task.save()
